2500e46036e745416f24ee0a95ecae63ce53bb3c
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2017-10-27 13:51:07 -0400

    install.sh finds proper path based on current name

--- install.sh
+++ install.sh
@@ -5,15 +5,23 @@
 
 
 
+## Wherever this repo exists will be set as target for symlinks, and get a sibling folder with _bak suffix for any displaced files
+this_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+dotfiles_dir=$this_path/home   # change this if you checked out repo under different name      
+backup_dir=${this_path}_bak      
 
-dotfiles_dir=~/dotfiles/home   # change this if you checked out repo under different name      
-backup_dir=~/dotfiles_bak       
+
+
+
+
+
 files=$(ls -A ${dotfiles_dir})   
 
-
+#########
+# show use potential impact before doing anything, this can be omitted.
 echo "Files and folders to symlink/overwrite:"
 for file in $files;do
-	echo -e "\t$file"
+    echo -e "\t$file"
 done
 read -rep $'Press [Enter] to continue these files, otherwise press CTRL+C:\n>'
 ##########
@@ -32,17 +40,17 @@ echo ""
 for file in $files; do
     echo -e "Creating symlink to $file in home directory."
     if [ -L ~/$file ];then
-    	target=$(stat -f %Y ~/$file)
-    	if [ "$dotfiles_dir/$file" == "$target" ];then
-    		echo -e "\tSymlink already exists, done."
-    		continue
-    	else
-    		echo -e "\tSymlink already points to $target, it will be replaced by path $dotfiles_dir/$file"
-    		rm ~/$file
-    	fi
+        target=$(stat -f %Y ~/$file)
+        if [ "$dotfiles_dir/$file" == "$target" ];then
+            echo -e "\tSymlink already exists, done."
+            continue
+        else
+            echo -e "\tSymlink already points to $target, it will be replaced by path $dotfiles_dir/$file"
+            rm ~/$file
+        fi
     elif [ -s ~/$file ]; then
-    	echo -e "\t Moving existing file to $backup_dir/$file"
-    	mv ~/$file $backup_dir/
+        echo -e "\t Moving existing file to $backup_dir/$file"
+        mv ~/$file $backup_dir/
     fi
     ln -s $dotfiles_dir/$file ~/$file
     echo -e "\t~/$file -> $dotfiles_dir/$file"
